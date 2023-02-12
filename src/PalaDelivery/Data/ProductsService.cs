3ecb1f62577130b7fb46186840caafb4719e9619
Tiago Pala <tiago_pala@outlook.com>
2023-02-12 17:37:12 -0300

    Remoção da página de previsão de tempo e adição da página de produtos.

---
+++ src/PalaDelivery/Data/ProductsService.cs
@@ -0,0 +1,26 @@
+﻿namespace PalaDelivery.Data
+{
+    public class ProductsService
+    {
+        private static readonly string[] ProductNames = new[]
+        {
+            "Pão de Queijo", "Pão de Mel", "Pão Doce", "Pão Francês"
+        };
+
+        public async Task<List<Product>> GetRandomProducts()
+        {
+            var random = new Random();
+
+            var products = new List<Product>();
+
+            foreach (var productName in ProductNames)
+            {
+                products.Add(new Product(productName, Math.Round(random.Next(5,10) + random.NextDouble(),2)));
+            }
+
+            var orderedProducts = products.OrderBy(p => p.Price).ToList();
+
+            return await Task.FromResult(orderedProducts);
+        }
+    }
+}
--- src/PalaDelivery/Program.cs
+++ src/PalaDelivery/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<ProductsService>();
 
 var app = builder.Build();
 
